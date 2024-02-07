fddf4163ac8b9326852b4f794ded3e0aa46f32bf
Eliel Martins <elieuprata@gmail.com>
2024-02-07 09:40:29 -0300

    feat: Implementing View for tag creator with Http Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,16 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+  '''
+    responsibility for interacting with HTTP
+  '''
+
+  def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+    body = http_request.body
+    product_code = body["product_code"]
+
+    print('estou na minha view')
+    print(product_code)
+
+    return HttpResponse(status_code=200, body={"hello": "world"})
